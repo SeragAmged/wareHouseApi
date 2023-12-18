aa2b28e118a05025b0e3d1cf9e60d01da1e2de77
SeragAmged <seragamged2002@gmail.com>
2023-12-18 14:36:35 +0200

    update

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 from fastapi import FastAPI
 from api.routes import *
+from utils.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
-
 app.include_router(router)
-
--- src/utils/models.py
+++ src/utils/models.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python3
 from datetime import datetime
-from sqlalchemy import Boolean, Column, Date, ForeignKey, Integer, String, Time
+import enum
+from sqlalchemy import Boolean, Column, Date, ForeignKey, Integer, String, Time, Enum
 from sqlalchemy.orm import relationship
+from database import Base
 
-from .database import Base
+
+class StatusEnum(enum.Enum):
+    available = "available"
+    out_of_service = "out of service"
+    job_assigned = "job assigned"
+    booked = "booked"
+    lended = "lended"
+    messing_item = "messing item"
+    out_of_calibration = "out of calibration"
+    calibration_due = "calibration due"
+    test_and_tag_due = "test and tag due"
+
+
+class CommentsEnum(enum.Enum):
+    needs_action = "needs_action"
+    normal = "normal"
 
 
 class Branch(Base):
     __tablename__ = "branch"
 
     id = Column(Integer, primary_key=True, autoincrement=True, index=True)
-    branch_name = Column(String, index=True)
+    name = Column(String, index=True, nullable=False)
 
     has = relationship("Employee", back_populates="works")
     items = relationship("Item", back_populates="branch_owner")
@@ -20,25 +37,26 @@ class Employee(Base):
     __tablename__ = "employee"
 
     id = Column(Integer, primary_key=True,
-                autoincrement=True, index=True)  # database id
+                autoincrement=True, index=True)
+    branch_id = Column(Integer, ForeignKey(Branch.id), nullable=False)
 
     sesa_id = Column(Integer, unique=True, index=True,
-                     nullable=False)  # company id
+                     nullable=False)
     first_name = Column(String, index=True, nullable=False)
     last_name = Column(String, index=True, nullable=False)
     email = Column(String,  unique=True, index=True, nullable=False)
 
     hashed_password = Column(String, nullable=False)
-    phone = Column(String, unique=True, nullable=False)
-    role = Column(String, nullable=False)
+    phone = Column(String, unique=True, nullable=True)
 
-    branch_id = Column(Integer, ForeignKey(Branch.id), nullable=False)
+    role = Column(String, default="SR", nullable=False)
 
     works_at = relationship("Branch", back_populates="has")
-    comments = relationship("Comment", back_populates="employee")
 
+    comments = relationship("Comment", back_populates="employee")
     check_out = relationship("CheckOut", back_populates="employee")
     check_in = relationship("CheckIn", back_populates="employee")
+    book = relationship("Book", back_populates="employee")
 
     add_item_detail_record = relationship(
         "AddItemDetailRecord", back_populates="employee")
@@ -49,39 +67,43 @@ class ItemDetail(Base):
     __tablename__ = "itemDetail"
 
     id = Column(Integer, primary_key=True,  autoincrement=True, index=True)
-    name = Column(String, index=True, nullable=False)
+    name = Column(String, index=True, nullable=False, unique=True)
     image_link = Column(
-        String, default="https://demofree.sirv.com/nope-not-here.jpg")
-    category = Column(String)  # TODO:nullable or no?
-    details = Column(String)  # TODO:nullable or no?
+        String, default="https://demofree.sirv.com/nope-not-here.jpg", nullable=False)
+    category = Column(String, nullable=True)
+    details = Column(String, nullable=True)
     quantity = Column(Integer, default=0, nullable=False)
-    data_sheet_link = Column(String)
+    data_sheet_link = Column(String, default="No pdf!", nullable=True)
 
     items = relationship("Item", back_populates="details")
     has_item_detail_record = relationship(
-        "ItemDetailsRecord", back_populates="add")
+        "AddItemDetailRecord", back_populates="item")
 
 
 class Item(Base):
     __tablename__ = "item"
 
     id = Column(Integer, primary_key=True,  autoincrement=True, index=True)
+    item_detail_id = Column(Integer, ForeignKey(ItemDetail.id), nullable=False)
+    branch_id = Column(Integer, ForeignKey(Branch.id), nullable=False)
+
     se_id = Column(Integer, unique=True, index=True, nullable=False)
     serial_number = Column(Integer, unique=True, nullable=False)
 
-    calibration_date = Column(Date)
-    out_of_calibration = Column(Boolean, default=False, nullable=False)
-    calibration_certificate_link = Column(String, nullable=False)
+    calibratable = Column(Boolean, nullable=False)
+    calibration_date = Column(Date, nullable=True)
+    out_of_calibration = Column(
+        Boolean, default=False, nullable=True)
+    calibration_certificate_link = Column(String, nullable=True)
 
-    # TODO: will implement enum for some categories
-    status = Column(String, nullable=False)
-    # TODO: check WhatsApp details
+    status = Column(Enum(StatusEnum),
+                    default=StatusEnum.available, nullable=False)
+
     work_order = Column(Integer, nullable=True)
+    jop_name = Column(String, nullable=True)
+
     company_lended = Column(String, nullable=True)
     booked = Column(Boolean, default=False, nullable=False)
-
-    item_detail_id = Column(Integer, ForeignKey(ItemDetail.id))
-    branch_id = Column(Integer, ForeignKey(Branch.id))
 
     detail = relationship("ItemDetail", back_populates="items")
     branch_owner = relationship("Branch", back_populates="items")
@@ -102,12 +124,13 @@ class Comment(Base):
                 autoincrement=True, index=True)
 
     comment = Column(String, nullable=False)
-    date = Column(Date, default=datetime.now().date(), nullable=False)
-    time = Column(Time, default=datetime.now().time(), nullable=False)
-    type = Column(String)  # TODO: implement enum types
+    date = Column(Date, default=datetime.utcnow().date(), nullable=False)
+    time = Column(Time, default=datetime.utcnow().time(), nullable=False)
+    type = Column(Enum(CommentsEnum),
+                  default=CommentsEnum.normal, nullable=False)
 
-    employee_id = Column(Integer, ForeignKey(Employee.id))
-    item_id = Column(Integer, ForeignKey(Item.id), index=True)
+    employee_id = Column(Integer, ForeignKey(Employee.id), nullable=False)
+    item_id = Column(Integer, ForeignKey(Item.id), index=True, nullable=False)
 
     item = relationship("Item", back_populates="comments")
     employee = relationship("Employee", back_populates="comments")
@@ -116,12 +139,12 @@ class Comment(Base):
 class Book(Base):
     __tablename__ = "book"
 
-    id = Column(Integer, primary_key=True,  autoincrement=True, index=True,)
+    id = Column(Integer, primary_key=True,  autoincrement=True, index=True)
     item_id = Column(Integer, ForeignKey(Item.id), index=True, nullable=False)
     employee_id = Column(Integer, ForeignKey(
         Employee.id), index=True, nullable=False)
     future_check_out_date = Column(Date, nullable=True)
-
+    booked_for_work_order = Column(Integer, nullable=False)
     item = relationship("Item", back_populates="book")
     employee = relationship("Employee", back_populates="book")
 
@@ -137,11 +160,11 @@ class CheckOut(Base):
         Employee.id), nullable=False)
 
     date = Column(Date, index=True,
-                  default=datetime.now().date(), nullable=False)
+                  default=datetime.utcnow().date(), nullable=False)
     time = Column(Time, index=True,
-                  default=datetime.now().time(), nullable=False)
+                  default=datetime.utcnow().time(), nullable=False)
 
-    estimated_Check_in_Date = Column(Date)
+    estimated_Check_in_Date = Column(Date, nullable=True)
 
     item = relationship("Item", back_populates="check_out")
     employee = relationship("Employee", back_populates="check_out")
@@ -158,9 +181,9 @@ class CheckIn(Base):
         Employee.id), nullable=False)
 
     date = Column(Date, index=True,
-                  default=datetime.now().date(), nullable=False)
+                  default=datetime.utcnow().date(), nullable=False)
     time = Column(Time, index=True,
-                  default=datetime.now().time(), nullable=False)
+                  default=datetime.utcnow().time(), nullable=False)
 
     item = relationship("Item", back_populates="check_in")
     employee = relationship("Employee", back_populates="check_in")
@@ -170,8 +193,9 @@ class AddItemDetailRecord(Base):
     __tablename__ = "addItemDetailRecord"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    employee_id = Column(Integer, ForeignKey(Employee.id), index=True)
-    tool_id = Column(Integer, ForeignKey(Item.id))
+    employee_id = Column(Integer, ForeignKey(
+        Employee.id), index=True, nullable=False)
+    tool_id = Column(Integer, ForeignKey(Item.id), nullable=False)
     date = Column(Date, nullable=False)
     time = Column(Time, nullable=False)
 
@@ -184,10 +208,13 @@ class AddItemRecord(Base):
     __tablename__ = "addItemRecord"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    employee_id = Column(Integer, ForeignKey(Employee.id), index=True)
-    item_id = Column(Integer, ForeignKey(Item.id))
+    employee_id = Column(Integer, ForeignKey(
+        Employee.id), index=True, nullable=False)
+    item_id = Column(Integer, ForeignKey(Item.id), nullable=False)
     date = Column(Date, nullable=False)
     time = Column(Time, nullable=False)
 
     item = relationship("Item", back_populates="add_item_record")
     employee = relationship("Employee", back_populates="add_item_record")
+
+
