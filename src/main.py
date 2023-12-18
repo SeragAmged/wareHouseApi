a193b87277f903cbb6f7ce07c9587a1263813e25
Serag el-dein Amged <71095930+SeragAmged@users.noreply.github.com>
2023-12-18 14:38:32 +0200

    temp

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 from fastapi import FastAPI
+from api.database import Base, engine
 from api.routes import *
 
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(router)
-
