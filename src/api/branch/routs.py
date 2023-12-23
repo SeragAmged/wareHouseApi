84b2c01cbbb076c9b2ec2f4ce6220b56b18955e9
Serag el-dein Amged <71095930+SeragAmged@users.noreply.github.com>
2023-12-24 01:12:37 +0200

    branch tag update

--- src/api/branch/routs.py
+++ src/api/branch/routs.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import Dict, List
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
@@ -10,6 +11,8 @@ from api.schemas import BranchCreate
 
 branch_router = APIRouter()
 
+tags: List[str | Enum] = ["branch"]
+
 # Dependency
 
 
@@ -21,22 +24,22 @@ def get_db():
         db.close()
 
 
-@branch_router.post('/branches', response_model=schemas.Branch)
+@branch_router.post('/branches', response_model=schemas.Branch, tags=tags)
 def create_branch(branch: BranchCreate, db: Session = Depends(get_db)):
     return cr.create_branch(db=db, branch=branch)
 
 
-@branch_router.get('/branches', response_model=List[schemas.Branch])
+@branch_router.get('/branches', response_model=List[schemas.Branch], tags=tags)
 def get_branches(db: Session = Depends(get_db)):
     return cr.get_branches(db=db)
 
 
-@branch_router.put('/branches', response_model=schemas.Branch)
-def update_branch(name: str, branch: BranchCreate, db: Session = Depends(get_db)):
+@branch_router.put('/branches', response_model=schemas.Branch, tags=tags)
+def update_branch(name: str, branch: BranchCreate, db: Session = Depends(get_db),):
     return cr.update_branch_by_name(db=db, name=name, branch=branch)
 
 
-@branch_router.delete('/branches', response_model=Dict)
+@branch_router.delete('/branches', response_model=Dict, tags=tags)
 def delete_branch(name: str, db: Session = Depends(get_db)):
     cr.delete_branch_by_name(db=db, name=name)
     return {"message": f"Branch {name} hade been deleted"}
