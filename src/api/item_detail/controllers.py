5d0fddc0c6fb89a4ec27e7df0d62f65f5dbdf884
Ahmed Abdelsalam <104328384+Ahmed5052@users.noreply.github.com>
2023-12-25 13:36:49 +0200

    update on function of add item if still not work take it and i will handle this but let me know

--- src/api/item_detail/controllers.py
+++ src/api/item_detail/controllers.py
@@ -1,16 +1,24 @@
 from typing import List
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from utils import models
 from api import schemas
 
+
+
 def add_item_details(db:Session,item_details:schemas.ItemDetailCreate) -> models.ItemDetail | None:
+    existing_item = db.query(models.ItemDetail).filter(models.ItemDetail.name == item_details.name).first()
+    if existing_item:
+        raise HTTPException(status_code=400, detail="Item with the same name already exists.")
+
+    # Create a new ItemDetail instance and add it to the database
     db_item_details = models.ItemDetail(**item_details.model_dump())
     db.add(db_item_details)
     db.commit()
     db.refresh(db_item_details)
     return db_item_details
 
-def get_item_details(db:Session,skip: int = 0, limit: int = 100) -> list[models.ItemDetail]:
+def get_item_details(db:Session,skip: int = 0, limit: int = 100) -> list[models.ItemDetail] | None:
     return db.query(models.ItemDetail).order_by(models.ItemDetail.name).offset(skip).limit(limit).all()
 
 def get_item_detail_by_id(db: Session, id: int) -> models.ItemDetail | None:
